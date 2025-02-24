f9cf256230aa4c04648454cae2a50e32e33ec5ad
Ryan Nalbach <103783337+Ryannalbach@users.noreply.github.com>
2025-02-23 17:38:08 -0800

    changes how the logic runs

--- numbergame.cpp
+++ numbergame.cpp
@@ -2,65 +2,75 @@
 * Program Name: Number Generator
 * Created Date: 2/22/2025
 * Created By: Ryan Nalbach
-* Purpose: TO generator a random number and have the user guess the number
-* Acknowledgements: Tony Gaddis C++ pearson book, professor Cayton Polen
+* Purpose: To generate a random number and have the user guess the number
+* Acknowledgements: Tony Gaddis C++ Pearson book, Professor Cayton Polen
 *******************************************************************************/
-#include <iostream> //to print to console and have input
-#include <iomanip> //to change configuration of output  
-#include <cstdlib>    // For rand() and srand() this will help generate the random number
-#include <ctime>      // For time() this will help generate the random number
 
-using namespace std; //this is the standard library
+#include <iostream>  // To print to console and have input
+#include <iomanip>   // To change configuration of output  
+#include <cstdlib>   // For rand() and srand() - generates the random number
+#include <ctime>     // For time() - helps seed the random number generator
+
+using namespace std; // This is the standard library
 
 /*******************************************************************************
 * Function Name: main()
 * Parameters: None
 * Return Value: int
-* Purpose: to generator a random number that the user guesses
+* Purpose: To generate a random number that the user guesses
 *******************************************************************************/
 
 int main() {
     // Seed the random number generator
-    srand(time(0)); //by using the time we change the random numbers without this it would generate the same random number
+    srand(time(0)); // Ensures a different random number each run
 
     // Generate a random number between 1 and 100
     int randomNumber = (rand() % 100) + 1; 
     int userGuess;
     int attempts = 5; // Number of chances
 
-    cout << "Welcome to the Number Guessing Game!" << endl; //this is the introduction to the game for the user to see
+    cout << "Welcome to the Number Guessing Game!" << endl;
     cout << "Please guess a number between 1 and 100." << endl;
-    cout << "You have " << attempts << " attempts to guess it correctly." << endl; //clarifying the attempts logic below, doing it this way uses less lines of code
+    cout << "You have " << attempts << " attempts to guess it correctly." << endl;
 
     // Loop to allow the user to guess
-    for (int i = 1; i <= attempts; i++) { //this is the logic for calculating the number of guesses left
-        do { //using the do allows the code to run once for the loop
-            cout << "Attempt " << i << ": Enter your guess: "; //this is the prompt for the user  to input a guess
-            cin >> userGuess; //this allows the user to eneter the guess
+    for (int i = 1; i <= attempts; i++) { // Loop for number of attempts
+        do { 
+            cout << "You have " << (attempts - i + 1) << " attempts remaining. Enter your guess: ";
+            cin >> userGuess;
+
+            // Check if input is invalid (not a number)
+            if (cin.fail()) { 
+                cout << "Invalid input! Please enter a number between 1 and 100." << endl;
+                cin.clear();  // Reset input state
+                cin.ignore(1000, '\n'); // Ignore extra characters in input
+                continue; // Restart the loop
+            }
 
             // Check if the guess is out of range
-            if (userGuess < 1 || userGuess > 100) { // the || acts as or statement
+            if (userGuess < 1 || userGuess > 100) { 
                 cout << "Invalid number! Please enter a number between 1 and 100." << endl;
-            }// output to show the user they have an invalid input
+            }
 
-        } while (userGuess < 1 || userGuess > 100); // Repeat until valid input is given
+        } while (cin.fail() || userGuess < 1 || userGuess > 100); // Ensure valid input
 
         // Check if the guess is correct
         if (userGuess == randomNumber) {
             cout << "Congratulations! You guessed the right number!" << endl;
             return 0; // Ends the program
         }
-        else if (userGuess > randomNumber) { //this continues if the guess isn't is high
-            cout << "your guess is too high! Try again." << endl;
+        else if (userGuess > randomNumber) { 
+            cout << "Your guess is too high! Try again." << endl;
         }
-        else { //this is for the guess being too low
-            cout << "your guess is too low! Try again." << endl;
+        else { 
+            cout << "Your guess is too low! Try again." << endl;
         }
     }
 
     // If user runs out of attempts
     cout << "Sorry, you've used all your attempts!" << endl;
-    cout << "The correct number was: " << randomNumber << endl; //this shows the user the number if they couldn't guess it
+    cout << "The correct number was: " << randomNumber << endl; // Display correct answer
 
-    return 0; //this ends the program running 
+    return 0; // Ends the program
 }
+
